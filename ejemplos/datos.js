48b5f113def615012742368174563e8279b51043
albvg <albavidalgimenezavg@alumnos.cei.es>
2023-12-14 19:03:05 +0100

    update

---
+++ ejemplos/datos.js
@@ -0,0 +1,29 @@
+const restaurantes = [
+    {nombre:'Burger King', 
+    imagen:'https://assets-global.website-files.com/631b4b4e277091ef01450237/65207c15448a6fdfb06c58e0_BK_Halloween_PR_Image_1%20(1)%20(1).jpg', 
+    descripcion:'Parrilleros desde 1954', 
+    contacto:{telefono:'111111111', direccion:'calle 1'}, 
+    id:'01', 
+    categoria:['Comida rápida', 'Comida a domicilio', 'Hamburguesas']},
+
+    {nombre:'Saona', 
+    imagen:'https://www.gruposaona.com/app/uploads/2021/11/210930-Saona-Cortes-1.jpg', 
+    descripcion:'Comida mediterranea para ti', 
+    contacto:{telefono:'222222222', direccion:'calle 2'}, 
+    id:'02', 
+    categoria:['Comida mediterranea', 'Comida a domicilio']},
+
+    {nombre:'Telepizza', 
+    imagen:'https://www.cronista.com/files/image/510/510334/63e26a5c9d82d.jpg', 
+    descripcion:'¿Tienes hambre? Pide online tu pizza favorita y disfruta de nuestra deliciosa comida a domicilio.', 
+    contacto:{telefono:'333333333', direccion:'calle 3'}, 
+    id:'03', 
+    categoria:['Pizza', 'Comida a domicilio']},
+    
+    {nombre:'Miss Sushi', 
+    imagen:'https://misssushi.es/wp-content/uploads/1.1.MENU-SUPREME-Miss-sushi-opti.jpg', 
+    descripcion:'Sushi de calidad al mejor precio', 
+    contacto:{telefono:'444444444', direccion:'calle 4'}, 
+    id:'04', 
+    categoria:['Sushi', 'Comida a domicilio']
+}];
