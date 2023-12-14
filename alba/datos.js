190396857a45244f5b29fb5c325432af2db42cc2
Arevi9 <148113389+Arevi9@users.noreply.github.com>
2023-12-14 19:03:10 +0100

    ws

---
+++ alba/datos.js
@@ -0,0 +1,58 @@
+const restaurantes = [{
+
+    id: "1",
+    imagen: "https://imag.bonviveur.com/pizza-margarita.jpg" ,
+    nombre: "La Playa",
+    descripcion: "Tu nueva pizzería al alcance de todos",
+    categoria: ["Pizza", " Pasta"],
+    icono2:"fa-solid fa-location-dot",
+    direccion: "Villa Azulejo, 12 (Valencia)",
+    icono:"fa-solid fa-phone",
+    telefono:"627 82 67 35",
+    }, {
+
+    id: "2",
+    imagen: "https://piccavey.com/wp-content/uploads/2018/11/Criolla-Valladolid-Spain-Tapas.jpg.webp" ,
+    nombre: "Bar Marcela",
+    descripcion: "Tu nueva pizzería al alcance de todos",
+    categoria: ["Typical spanish", " Croquetas"],
+    icono2:"fa-solid fa-location-dot",
+    direccion: "Calle La Glorieta, 98 (Valencia)",
+    icono:"fa-solid fa-phone",
+    telefono:"926 67 54 25",
+    },{
+
+    id: "3",
+    imagen: "https://media.timeout.com/images/105971281/image.jpg" ,
+    nombre: "붉은 입술",
+    descripcion: "안녕하세요",
+    categoria: "Hotpot",
+    icono2:"fa-solid fa-location-dot",
+    direccion: "Plaza de la Marmota, 52 (Valencia)",
+    icono:"fa-solid fa-phone",
+    telefono:"876 52 63 51",
+    },{
+
+    id: "4",
+    imagen: "https://www.mentta.com/blog/wp-content/uploads/2021/12/dulces.jpg" ,
+    nombre: "María Antonieta",
+    descripcion: "Endulza tu día con nuestros postres a la carta",
+    categoria: "Dulces",
+    icono2:"fa-solid fa-location-dot",
+    direccion: "Avenida Nazal, 34 (Valencia)",
+    icono:"fa-solid fa-phone",
+    telefono:"967 55 46 42",
+    },{
+
+    id: "5",
+    imagen: "https://asadorlabellota.com/wp-content/uploads/2022/01/20220112_190227_0000.png" ,
+    nombre: "Restaurante Rocinante",
+    descripcion: "Ñam Ñam",
+    categoria: "Carne a la brasa",
+    icono2:"fa-solid fa-location-dot",
+    direccion: "Calle La Glorieta, 51 (Valencia)",
+    icono:"fa-solid fa-phone",
+    telefono:"652 89 06 12",
+    },
+
+    ]
